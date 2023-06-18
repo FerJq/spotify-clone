6a019fe755bc6fecd1b96c54f99cc17d369bab84
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-06-17 23:53:21 -0400

    concat.api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,10 +1,16 @@
 import {createApi, fetchBaseQuary} from '@reduxjs/toolkit/query/react'
 
-const options = { method: 'GET', headers: { 'X-RapidAPI-Host': '' } };
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '8f7e390fb4msh1a60784d6969a9ep1a49d5jsne71b2a788436',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+  }
+};
 
 fetch('https://echo.paw.cloud/', options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
 
-  export const shazamCoreApi =
+  export const shazamCoreApi = createApi({reducerPath: 'shazamCoreApi'})
