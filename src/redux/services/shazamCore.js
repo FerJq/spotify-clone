85c5db26e359901b033bb63fc512813d434756ac
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-07-02 16:12:11 -0400

    added new call axios api to get geo location and get the songs around you component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -36,6 +36,9 @@ export const shazamCoreApi = createApi({
     getArtistDetails: builder.query({
       query: (artistId) => `/artists/details?artist_id=${artistId}`,
     }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
   }),
 });
 
@@ -44,4 +47,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
