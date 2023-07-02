1bc18a62e5c576dc7a16d3d6c27f213dea360716
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-07-02 19:15:59 -0400

    searchBar

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,26 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { Error, Loader, ArtistCard } from '../components';
+import { useGetTopArtistsQuery } from '../redux/services/shazamCore';
+
+const TopArtists = () => {
+  const [activeSong, isPlaying] = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopArtistsQuery();
+
+  if (isFetching) return <Loader title="Loading TOP ARTISTS" />;
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Artists
+        <span className="font-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track, i) => (
+          <ArtistCard key={track.key} track={track} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopArtists;
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
